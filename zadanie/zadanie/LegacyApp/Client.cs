2eeda429a7265510f2b02a3c70401ffd2a2405cc
s23346 <s23346@pjwstk.edu.pl>
2024-04-04 15:44:29 +0200

    dodano komentarz

--- zadanie/zadanie/LegacyApp/Client.cs
+++ zadanie/zadanie/LegacyApp/Client.cs
@@ -3,6 +3,7 @@
     public class Client
     {
         public string Name { get; internal set; }
+        // komentarz
         public int ClientId { get; internal set; }
         public string Email { get; internal set; }
         public string Address { get; internal set; }
